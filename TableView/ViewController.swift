bdb0428ebdde1bc08e8f66d85cb3f96af20375b6
Alumno <ulsa@iMacD110.local>
2018-09-18 18:49:16 -0700

    Primera Version

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -8,7 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let alumnos = ["Rene", "Minneth", "Betin", "David", "Pablo", "Fernanda", "Sofia", "Rafael", "Mariana", "Any", "Relleno", "Iram", "Benja", "Memo", "Emiliano", "Ivan", "Joel", "Martita", "Betin2", "Gato", "Perla", "Perla", "Wero", "Mueble este", "Enrique Peña Nieto", "AMLO", "La Virgen de Ciudad Obregón", "Killaperro"]
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return alumnos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celdaAlumno = tableView.dequeueReusableCell(withIdentifier: "cellAlumno")
+        celdaAlumno?.textLabel?.text = alumnos[indexPath.row]
+        
+        return celdaAlumno!
+        
+        
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
